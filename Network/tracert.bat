dcd465a13e5924886fcd69f79bdcf20f65223eaf
Matthéo Le Fur <mattheo.lefur41@gmail.com>
2022-08-05 22:16:34 +0200

    Update tracert.bat

--- Network/tracert.bat
+++ Network/tracert.bat
@@ -2,7 +2,7 @@
 
 @echo off
 title Network Tracer
-Set /p Host= Please Enter Hostname or IP Address: 
+Set /p Host= Enter Hostname or IP Address: 
 if defined Host (
    powershell -Command tracert -d %Host% 
 )
