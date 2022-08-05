1e1fba3a8611a1afab37d1bf211c33403c05408b
Matthéo Le Fur <mattheo.lefur41@gmail.com>
2022-08-05 22:05:22 +0200

    Update restart_network.bat

--- Network/restart_network.bat
+++ Network/restart_network.bat
@@ -2,7 +2,6 @@
 
 @echo off
 
-
 dism >nul 2>&1 || (echo This script must be Run as Administrator. && pause && exit /b 1)
 for /f "tokens=3,*" %%i in ('netsh int show interface^|find "Connected"') do (
 	netsh int set interface name="%%j" admin="disabled" >nul 2>&1
