87e496bcf41e586dc6fc3ac37bd3ad96bf93a2ee
Matthéo Le Fur <mattheo.lefur41@gmail.com>
2022-08-05 22:04:52 +0200

    Update reset_firewall.bat

--- Network/reset_firewall.bat
+++ Network/reset_firewall.bat
@@ -1,10 +1,8 @@
 :: Reset Firewall (Beta)
 
 @echo off
-setlocal ENABLEDELAYEDEXPANSION
 
 dism >nul 2>&1 || (echo This script must be Run as Administrator. && pause && exit /b 1)
-
 netsh advfirewall set allprofiles state off >nul 2>&1
 netsh advfirewall firewall delete rule all
 netsh advfirewall set allprofiles firewallpolicy blockinbound,blockoutbound >nul 2>&1
